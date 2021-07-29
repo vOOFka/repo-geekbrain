ff06efcc6a99ba8f71cca22ccae8c6dc68c9e3d1
vOOFka <exit551@ya.ru>
2021-07-29 03:07:35 +0300

    lesson 4

--- vkontakteVS/vkontakteVS/Scenes/UserFriends/Cells/FriendTableViewCell.swift
+++ vkontakteVS/vkontakteVS/Scenes/UserFriends/Cells/FriendTableViewCell.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class FriendTableViewCell: UITableViewCell {
     
+  
     //MARK: - Outlets
-    @IBOutlet weak var friendImage: UIImageView!
-    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendImage: UIImageView! {
+        didSet {
+            friendImage.layer.cornerRadius = friendImage.frame.size.height / 2
+            friendImage.layer.masksToBounds = true
+//            friendImage.layer.borderColor = UIColor.brown.cgColor
+//            friendImage.layer.borderWidth = 10
+        }
+    }
     
+    @IBOutlet weak var friendName: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
