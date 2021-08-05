dd64a68cc11d85c21617585ca29cd09d6a82c0ed
vOOFka <exit551@ya.ru>
2021-08-06 00:30:54 +0300

    edit news layout

--- vkontakteVS/vkontakteVS/Scenes/UserNews/UserNewsViewController.swift
+++ vkontakteVS/vkontakteVS/Scenes/UserNews/UserNewsViewController.swift
@@ -18,11 +18,14 @@ class UserNewsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //register Header of cell
+        tableView.register(UINib(nibName: cellID, bundle: nil), forCellReuseIdentifier: cellID)
+//        tableView.estimatedRowHeight = 250.0
+//        tableView.rowHeight = UITableView.automaticDimension
     }
-  
 }
 
-extension UserNewsViewController: UITableViewDelegate, UITableViewDataSource {
+extension UserNewsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
@@ -33,5 +36,10 @@ extension UserNewsViewController: UITableViewDelegate, UITableViewDataSource {
         }
         return cell
     }
-    
+}
+
+extension UserNewsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 305
+    }
 }
