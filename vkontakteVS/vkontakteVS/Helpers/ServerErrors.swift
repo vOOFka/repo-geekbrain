24735828c5804395924a4551b47bd8ea1b542248
Vladimir Sirel <exit551@ya.ru>
2021-11-15 00:51:32 +0300

    add PromiseKit and Promise concept (Get, Parsing, Save to Realm, Get from Realm) in GroupsViewController

---
+++ vkontakteVS/vkontakteVS/Helpers/ServerErrors.swift
@@ -0,0 +1,14 @@
+//
+//  ServerErrors.swift
+//  vkontakteVS
+//
+//  Created by Home on 14.11.2021.
+//
+
+import Foundation
+
+enum ServerErrors: Error {
+    case DataIsNil
+    case RequestError
+    case ParsedError
+}
